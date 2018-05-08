37acfe613483c35886a5d28469c48d30282f614b
DESKTOP-K7OPVS1\katie <katie.sang.park@gmail.com>
2018-05-07 18:38:22 -0700

    test

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -22,6 +22,7 @@ public class DetailActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_detail);
         ImageView ingredientsIv = findViewById(R.id.image_iv);
         Intent intent = getIntent();
